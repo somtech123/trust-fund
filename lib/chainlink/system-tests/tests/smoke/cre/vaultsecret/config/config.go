5b6d4a7071f09dfa28634ed2ee8682ba75751933
somtech123 <oscardre@gmail.com>
2026-04-03 20:29:12 -0700

    fixed .gitmodules files

--- lib/chainlink/system-tests/tests/smoke/cre/vaultsecret/config/config.go
+++
@@ -1,7 +0,0 @@
-package config
-
-type Config struct {
-	SecretKey       string `yaml:"secretKey"`
-	SecretNamespace string `yaml:"secretNamespace"`
-	ExpectNotFound  bool   `yaml:"expectNotFound"`
-}
